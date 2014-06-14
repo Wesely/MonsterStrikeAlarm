00d6fb7abddb85fff4b99481a6b80dfe0ae1df11
Bruce Kuo <bruce3557@gmail.com>
2014-06-14 23:24:07 +0800

    Override click link action

--- src/tw/wesely/archives/ArchiveDetailActivity.java
+++ src/tw/wesely/archives/ArchiveDetailActivity.java
@@ -1,5 +1,12 @@
 package tw.wesely.archives;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import tw.wesely.mstrikealarm.R;
 import tw.wesely.mstrikealerm.MainActivity;
 import android.app.Activity;
@@ -7,13 +14,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
 
 public class ArchiveDetailActivity extends Activity {
 	Context ctx;
 	String monsterID;
-
+	MonsterWebView mwv = null;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -24,10 +34,23 @@ public class ArchiveDetailActivity extends Activity {
 		monsterID = intent.getExtras().getString("monsterID");
 		Log.d("ArchiveDetailActivity", "reciving monsterID = No." + monsterID);
 
-		MonsterWebView mwv = new MonsterWebView(ctx);
+		mwv = new MonsterWebView(ctx);
 		LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT,
 				LayoutParams.MATCH_PARENT);
 		params.setMargins(0, 0, 0, 0);
+		
+		mwv.setWebViewClient(new WebViewClient()  {
+			@Override
+			public boolean shouldOverrideUrlLoading(WebView view, String url){
+				Log.d("monster client", "set client");
+				if( url.matches(".*/monster/.*") ) {
+					Log.d("monster client", "load data");
+					mwv.loadMonsterSection(url);
+					return true;
+				}
+				return false;
+			}
+		});
 		mwv.loadMonsterById(monsterID);
 		// mwv.setLayoutParams(params);
 		LinearLayout container = (LinearLayout) findViewById(R.id.container);
